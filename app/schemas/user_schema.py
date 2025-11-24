c142ef1f3a79f9843fe6072764bf222db59d62c8
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-24 19:23:07 +0100

    create user schemas

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -5,3 +5,29 @@ from typing import Optional
 class UserBase(BaseModel):
     username: str
     email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserSchema(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str
+
+
+class TraductionRequest(BaseModel):
+    text: str
+    language: str
