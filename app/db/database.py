733d970327d6ca1d217c8b2387ad4fc213d80583
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-24 19:22:36 +0100

    Data base configuration

--- app/db/database.py
+++ app/db/database.py
@@ -7,10 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:123456@{DATABASE_HOST}:5432/db_name"
 
-# Create the engine
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
